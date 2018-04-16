2c77c3d4dbe53890380ce0ee08cca50f3e2b5b52
Geoffrey Ritchey <geoffvr@live.com>
2018-04-16 13:43:51 -0500

    Update dailyApp.py

--- dailyApp.py
+++ dailyApp.py
@@ -1,7 +1,7 @@
 import requests
 import pyttsx3
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q=Lubbock&units=imperial&appid=9563a9db0bb1d5112facd4fd04c0279e'
+url = 'http://api.openweathermap.org/data/2.5/weather?q=Lubbock&units=imperial&appid=API KEY GOES HERE'
 
 
 def weather():
@@ -10,7 +10,7 @@ def weather():
     description = json_data['weather'][0]['description']
     temp = json_data['main']['temp']
     temp_max = json_data['main']['temp_max']
-    sentence = "Good morning Geoffrey! The forecast today will be {}. The temperature is currently {} degrees Fahrenheit, with a high of {} degrees today".format(
+    sentence = "Good morning! The forecast today will be {}. The temperature is currently {} degrees Fahrenheit, with a high of {} degrees today".format(
         description, int(temp), int(temp_max))
     print(sentence)
 
